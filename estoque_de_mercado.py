8622a97dc4a1dfdf691f661542dee7ee0ae22d06
ArthurTakamori <arthurguilhermes@gmail.com>
2024-02-08 16:28:57 -0300

    ADD COMMENTS ABOUT STRUCTURE

---
+++ estoque_de_mercado.py
@@ -0,0 +1,25 @@
+# 1. Ter um estoque de produtos em alguma(s) estrutura(s) de dados que permita a alteração deste estoque;
+    # 1.1 Um estoque consiste em uma relação de Produto e sua Quantidade Disponível
+    # 1.2 Precisamos de estruturas de dados e uma relação entre elas ou uma estrutura de dados única que já permita estabelecer essa relação nativamente;
+    # 1.3 Um estoque é algo dinâmico, pode ter incremento ou decremento de quantidades e até exaustão de um de seus produtos listados.
+    # 1.4 Criar uma lista de produtos, incluindo dicionário com as chaves nome_item, preço_item, quantidade_item e valores pré-fixados que podem ser alterados caso tenha necessidade.
+
+# 2. Print do estoque para o cliente;
+    # 2.1 Mostrar a lista de itens disponíveis ao cliente.
+
+# 3. Dado o input do cliente de qual produto ele quer, perguntar a quantidade que quer.
+
+    # 3.1 Perguntar qual item ele gostaria de adicionar ao carrinho
+    # 3.2 Perguntar a quantidade que ele gostaria de adicionar
+    # 3.3 Condicionar saída de itens do estoque geral disponibilizado
+    # 3.4 Enviar item ao carrinho de compras
+
+# 4. Reconciliar o estoque com base no pedido do cliente;
+    # 4.1 - Condicionar saída de item automáticamente do estoque ao ser adicionado ao carrinho.
+
+# 5. repetir requisitos 2, 3 e 4 até que o cliente forneça um input que cancele termine a execução do "carrinho de compras";
+    # 5.1 - Repetir requisitos até que o cliente digite se deseja adicionar mais itens ou não
+
+# 6. Finalizar printando o que tem no estoque ainda depois da compra do cliente, informando o valor total da compra do cliente, seus itens e quantidades
+    # 6.1 - Printar a lista de itens comprados com quantidades e valores somados e multiplicados individualmente 
+    # 6.2 - Ao final, printar também o valor da compra total.
